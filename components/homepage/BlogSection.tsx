5ca652b658cc0df4d7f8428b2e1ef48032cce8ef
Nabil Ridhwan <nabridhwan@gmail.com>
2022-09-26 12:09:54 +0800

    feat: :bug: Fix fetching blog error

--- components/homepage/BlogSection.tsx
+++ components/homepage/BlogSection.tsx
@@ -8,66 +8,89 @@ import fetchBlogItems, {
 
 export default function BlogSection() {
 	const [blogItems, setBlogItems] = useState<BlogItem[] | []>([]);
+	const [error, setError] = useState(false);
 
 	useEffect(() => {
 		(async () => {
-			const posts = await fetchBlogItems();
-			setBlogItems(posts);
+			try {
+				const posts = await fetchBlogItems();
+				setBlogItems(posts);
+				setError(false);
+			} catch (error) {
+				setError(true);
+			}
 		})();
 	}, []);
 
 	return (
-		<div>
+		<div className="w-full">
 			{/* Grid */}
 
-			<div className="grid md:grid-cols-2 gap-5">
-				{blogItems.length > 0 &&
-					blogItems &&
-					blogItems.map((item, i) => (
-						<motion.div
-							key={i}
-							whileHover={{
-								scale: 1.03,
-								cursor: 'pointer',
-							}}
-							whileTap={{ scale: 0.9 }}
-							onClick={() => {
-								window.location.href = item.url;
-							}}
-							className="relative rounded-xl border border-white/20"
-						>
-							<img
-								src={item.coverImage}
-								className="rounded-xl"
-								alt="Album cover image"
-							/>
+			{!error && (
+				<div className="grid md:grid-cols-2 gap-5">
+					{blogItems.length > 0 &&
+						blogItems &&
+						blogItems.map((item, i) => (
+							<motion.div
+								key={i}
+								whileHover={{
+									scale: 1.03,
+									cursor: 'pointer',
+								}}
+								whileTap={{ scale: 0.9 }}
+								onClick={() => {
+									window.location.href = item.url;
+								}}
+								className="relative rounded-xl border border-white/20"
+							>
+								<img
+									src={item.coverImage}
+									className="rounded-xl"
+									alt="Album cover image"
+								/>
 
-							{/* Gradient overlay */}
-							<div className="absolute rounded-xl w-full h-full bg-gradient-to-b from-transparent to-black top-0 left-0" />
+								{/* Gradient overlay */}
+								<div className="absolute rounded-xl w-full h-full bg-gradient-to-b from-transparent to-black top-0 left-0" />
 
-							{/* Text content */}
-							<div className="absolute bottom-5 px-4">
-								<h1 className="text-lg lg:text-3xl font-bold leading-snug">
-									{item.title}
-								</h1>
+								{/* Text content */}
+								<div className="absolute bottom-5 px-4">
+									<h1 className="text-lg lg:text-3xl font-bold leading-snug">
+										{item.title}
+									</h1>
 
-								{/* Bottom items */}
-								<div className="flex justify-between my-2">
-									<p className="font-light">
-										{DateTime.fromISO(
-											item.dateAdded
-										).toRelative()}
-									</p>
+									{/* Bottom items */}
+									<div className="flex justify-between my-2">
+										<p className="font-light">
+											{DateTime.fromISO(
+												item.dateAdded
+											).toRelative()}
+										</p>
 
-									<p className="font-light">
-										Read More
-										<IoLink className="inline ml-1 -rotate-45" />
-									</p>
+										<p className="font-light">
+											Read More
+											<IoLink className="inline ml-1 -rotate-45" />
+										</p>
+									</div>
 								</div>
-							</div>
-						</motion.div>
-					))}
-			</div>
+							</motion.div>
+						))}
+				</div>
+			)}
+
+			{error && (
+				<p className="muted text-center py-10 text-sm">
+					There was an error fetching the blog items. You can just
+					view my blog on{' '}
+					<a
+						href="https://blog.nabilridhwan.com"
+						target="_blank"
+						rel="noreferrer"
+						className="text-white underline"
+					>
+						https://blog.nabilridhwan.com
+					</a>
+				</p>
+			)}
 		</div>
 	);
 }
