39cb47d57e3c693afb4a93e73348162b8140f4aa
Nabil Ridhwan <nabridhwan@gmail.com>
2022-12-13 23:06:10 +0800

    feat: :sparkles: deploy fix

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,13 @@
 export default function NotFoundPage() {
-	return <h1>Not found</h1>;
+	return (
+		<div className="flex items-center justify-center my-56 text-center ">
+			<div className="space-y-4">
+				<h1 className="text-7xl">Not found</h1>
+
+				<p className="text-lg text-muted">
+					Is there anything here? HelllllooooooooOOOO!
+				</p>
+			</div>
+		</div>
+	);
 }
