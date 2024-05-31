f7c82a061f816af60345a494e8f20fe6b41bad1c
Nabil Ridhwan <nabridhwan@gmail.com>
2024-05-31 18:11:07 +0800

    fix: remove loading spinner

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -1,14 +1,15 @@
-import { Triangle } from 'react-loader-spinner';
+// import { Triangle } from 'react-loader-spinner';
 const LoadingSpinner = () => {
-	return (
-		<Triangle
-			height="80"
-			width="80"
-			color="white"
-			ariaLabel="triangle-loading"
-			visible={true}
-		/>
-	);
+    return (
+        <></>
+        // <Triangle
+        // 	height="80"
+        // 	width="80"
+        // 	color="white"
+        // 	ariaLabel="triangle-loading"
+        // 	visible={true}
+        // />
+    );
 };
 
 export default LoadingSpinner;
