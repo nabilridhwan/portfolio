54719afa65a1822b25fb279dcb2cff08bfc46401
Nabil Ridhwan <nabridhwan@gmail.com>
2025-05-24 08:17:10 +0800

    refactor: improve formatting and readability of Testimonial component

--- components/homepage/Testimonial.tsx
+++ components/homepage/Testimonial.tsx
@@ -1,26 +1,32 @@
 import Image from "next/image";
 
 interface TestimonialComponentProps {
-    name: string;
-    position: string;
-    testimonial: string;
-    image: string;
+	name: string;
+	position: string;
+	testimonial: string;
+	image: string;
 }
 
 export default function Testimonial(props: TestimonialComponentProps) {
+	return (
+		<div>
+			<p className={"italic font-bold"}>
+				&quot;{props.testimonial}&quot;
+			</p>
 
-    return (
-        <div>
-            <p className={'italic font-bold'}>&quot;{props.testimonial}&quot;</p>
-
-            <div className="mt-3 flex items-center gap-2">
-                <Image width={40} height={40} src={props.image} alt={props.name} className="rounded-full"/>
-                <div className={'opacity-50'}>
-                    <p>{props.name}</p>
-                    <p>{props.position}</p>
-                </div>
-            </div>
-        </div>
-    )
-
+			<div className="mt-3 flex items-center gap-2">
+				<Image
+					width={40}
+					height={40}
+					src={props.image}
+					alt={props.name}
+					className="rounded-full"
+				/>
+				<div className={"opacity-50"}>
+					<p>{props.name}</p>
+					<p className="text-sm opacity-75">{props.position}</p>
+				</div>
+			</div>
+		</div>
+	);
 }
