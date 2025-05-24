57349ca0e6437866e8451b3cdefe83b49c77cc90
Nabil Ridhwan <nabridhwan@gmail.com>
2025-05-24 08:16:57 +0800

    refactor: update ProjectItem component for improved readability and consistency

--- components/projects/ProjectItem.tsx
+++ components/projects/ProjectItem.tsx
@@ -1,42 +1,45 @@
 import Image from "next/image";
 import Link from "next/link";
-import {IoLinkSharp} from "react-icons/io5";
-import {Project} from "../../types/Project";
+import { TbExternalLink } from "react-icons/tb";
+import { Project } from "../../types/Project";
 
+const ProjectItem = ({
+	image,
+	title,
+	description,
+	technologies,
+	link,
+}: Project) => {
+	return (
+		<div className="space-y-2">
+			<Image
+				src={require(`../../public/projects/${image}`)}
+				alt="similarify"
+				className="rounded-3xl"
+			/>
 
-const ProjectItem = ({image, title, description, technologies, link}: Project) => {
-    return (
-        <div className="space-y-2">
-            <Image
-                src={require(`../../public/projects/${image}`)}
-                alt="similarify"
-                className="rounded-3xl"
-            />
+			<h3 className="text-2xl font-black">{title}</h3>
 
-            <section className={'flex flex-row flex-wrap gap-2'}>
-                {technologies.map((tech, index) => (
-                    <p
-                        key={index + tech}
-                        className="text-sm bg-primarylight px-2 py-1 my-0 rounded-full"
-                    >
-                        {tech}
-                    </p>
-                ))}
-            </section>
+			<section className={"flex flex-row flex-wrap gap-2"}>
+				{technologies.map((tech, index) => (
+					<p
+						key={index + tech}
+						className="text-opacity-75 text-xs bg-primarylight px-2 py-1 my-0 rounded-full"
+					>
+						{tech}
+					</p>
+				))}
+			</section>
+			<p className="muted">{description}</p>
 
-            <h3 className="text-2xl font-black">{title}</h3>
-            <p className="muted">
-                {description}
-            </p>
-
-            <Link href={link}>
-							<span className="flex my-2 items-center gap-2 cursor-pointer">
-								<IoLinkSharp className="-rotate-45"/>
-                                View Project
-							</span>
-            </Link>
-        </div>
-    )
-}
+			<Link href={link}>
+				<span className="flex my-3 items-center gap-2 cursor-pointer">
+					<TbExternalLink />
+					View Project
+				</span>
+			</Link>
+		</div>
+	);
+};
 
 export default ProjectItem;
