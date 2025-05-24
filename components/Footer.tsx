d4c1ad4b62a343cfac6a2313ab8e88d2181dfcd4
Nabil Ridhwan <nabridhwan@gmail.com>
2025-05-24 08:17:04 +0800

    fix: update footer text for clarity and consistency

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 
 export default function Footer() {
-    return (
-        <footer className="mt-[200px] mb-[90px] text-sm">
-            <div className="flex justify-center gap-3 text-muted italic">
-                <Link href="https://github.com/nabilridhwan/portfolio">
-                    this site is designed and coded with ❤️&nbsp;&nbsp;by Nabil
-                </Link>
-            </div>
-        </footer>
-    );
+	return (
+		<footer className="mt-[200px] mb-[90px] text-sm">
+			<div className="flex justify-center gap-3 text-muted italic">
+				<Link href="https://github.com/nabilridhwan/portfolio">
+					Designed and built with ❤️&nbsp;&nbsp;by Nabil
+				</Link>
+			</div>
+		</footer>
+	);
 }
